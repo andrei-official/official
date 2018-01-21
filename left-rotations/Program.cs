345d5bac638d9f3ba0143e00dfc0cca7d81c4563
Andrei Scripniciuc <andrei.scripniciuc@gmail.com>
2018-01-21 10:26:32 -0800

    implemented trie

--- left-rotations/Program.cs
+++ left-rotations/Program.cs
@@ -3,22 +3,10 @@ using System.Text;
 
 namespace left_rotations {
     class Program {
-        static void Main (string[] args) {
-            var input = "12345";
-            Console.WriteLine (LeftRotation (input, 4));
-        }
-
-        static string LeftRotation (string input, int rotations)
+        static void Main (string[] args)
         {
-            var firstItem = rotations % input.Length;
-            var result = new StringBuilder (input.Length);
-            result.Append (input[firstItem]);
-            for (var i = firstItem + 1; i < firstItem + input.Length; i++) {
-                result.Append (' ');
-                result.Append (input[i % input.Length]);
-            }
-
-            return result.ToString ();
+            var root = new Trie();
+            root.Insert("andrei");
         }
     }
 }
---
+++ tries/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new TrieNode();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line))
+            {
+                var input = line.Split(' ');
+                switch(input[0])
+                {
+                    case "a":
+                        root.Add(input[1]);
+                        break;
+                    case "f":
+                        Console.WriteLine(root.CountOccurences(input[1]));
+                        break;
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+
+    public class TrieNode
+    {
+        public TrieNode()
+        {
+            Children = new Dictionary<char, TrieNode>();
+        }
+
+        public Dictionary<char, TrieNode> Children { get; }
+
+        private bool IsWordEnding { get; set; }
+
+        public void Add(string value)
+        {
+            var node = this;
+            var i = 0;
+
+            // look up
+            while (i < value.Length)
+            {
+                if (node.Children.ContainsKey(value[i]))
+                {
+                    node = node.Children[value[i]];
+                    i++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // insert
+            while (i < value.Length)
+            {
+                node.Children.Add(value[i], new TrieNode());
+                node = node.Children[value[i]];
+                i++;
+            }
+
+            node.IsWordEnding = true;
+        }
+
+        public int CountOccurences(string value)
+        {
+            var node = this;
+            var i = 0;
+            // look up
+            while (i < value.Length)
+            {
+                if (node.Children.ContainsKey(value[i]))
+                {
+                    node = node.Children[value[i]];
+                    i++;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            // count
+            var toProcess = new Stack<TrieNode>();
+            var result = 0;
+            toProcess.Push(node);
+            while (toProcess.Count > 0)
+            {
+                var current = toProcess.Pop();
+                if (current.IsWordEnding)
+                {
+                    result++;
+                }
+                foreach (var child in current.Children)
+                {
+                    toProcess.Push(child.Value);
+                }
+            }
+
+            return result;
+        }
+    }
+}
